7b94e84999e591d443da9ef58afc8e9f2f188d85
Michal Bukovsky <michal.bukovsky@firma.seznam.cz>
2012-10-15 10:36:34 +0200

    #! async hint

---
+++ include/mcache/async.h
@@ -0,0 +1,40 @@
+/*
+ * FILE             $Id: $
+ *
+ * DESCRIPTION      Asynchronous client implementation.
+ *
+ * PROJECT          Seznam memcache client.
+ *
+ * AUTHOR           Michal Bukovsky <michal.bukovsky@firma.seznam.cz>
+ *
+ * Copyright (C) Seznam.cz a.s. 2012
+ * All Rights Reserved
+ *
+ * HISTORY
+ *       2012-10-04 (bukovsky)
+ *                  First draft.
+ */
+
+#ifndef MCACHE_ASYNC_H
+#define MCACHE_ASYNC_H
+
+// someone needs it?
+
+//int x() {
+//    mc::client_t c(a, c1, c2);
+//
+//    c.response_storage = xy;
+//    const response_t &r1 = c.get(100);
+//    if (response_t::wait_for_response != r1) throw 1;
+//    if (response_t::wait_for_response != c.get(101)) throw 1;
+//    if (response_t::wait_for_response != c.get(102)) throw 1;
+//    if (response_t::wait_for_response != c.get(103)) throw 1;
+//    if (response_t::wait_for_response != c.get(104)) throw 1;
+//    if (response_t::wait_for_response != c.get(105)) throw 1;
+//    c.wait_for_response();
+//
+//}
+
+
+#endif /* MCACHE_ASYNC_H */
+
